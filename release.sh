ae0059eac8bb4e959c01b190a351f3378a0b7a01
Mihai <amihaiemil@gmail.com>
2020-12-27 19:03:26 +0200

    Update release.sh

--- release.sh
+++ release.sh
@@ -32,7 +32,7 @@ echo "NEXT VERSION IS"
 echo $NEXT_VERSION
 
 mvn versions:set -DnewVersion=${tag}
-mvn clean deploy -PsignArtifactsGpg,deployToGithubPackages --settings /home/r/settings.xml
+mvn clean deploy -e -X -PsignArtifactsGpg,deployToGithubPackages --settings /home/r/settings.xml
 sed -i "s/CLI, version \`.*\`/CLI, version \`${tag}\`/" README.md
 
 mvn versions:set -DnewVersion=${NEXT_VERSION}
